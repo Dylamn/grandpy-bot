88867e943b075218d08b53943d4462a0deb9e19a
Dylan <dylan.vallee@hotmail.com>
2021-07-22 11:56:29 +0200

    Move `dotTyping` keyframe from `tailwind.css` to `tailwind.config.js` file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
 module.exports = {
   purge: [
     './grandpybot/templates/**/*.html',
-    './grandpybot/static/js/message.js',
+    './grandpybot/static/js/*.js',
   ],
   darkMode: 'class', // false, 'media' or 'class'
   theme: {
     extend: {
+      keyframes: {
+        dotTyping: {
+          '0%': {
+            boxShadow: '9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff, 10014px 0 0 0 #9880ff;'
+          },
+          '16.667%': {
+            boxShadow: '9984px -10px 0 0 #9880ff, 9999px 0 0 0 #9880ff, 10014px 0 0 0 #9880ff;'
+          },
+          '33.333%': {
+            boxShadow: '9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff, 10014px 0 0 0 #9880ff;'
+          },
+          '50%': {
+            boxShadow: '9984px 0 0 0 #9880ff, 9999px -10px 0 0 #9880ff, 10014px 0 0 0 #9880ff;'
+          },
+          '66.667%': {
+            boxShadow: '9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff, 10014px 0 0 0 #9880ff;'
+          },
+          '83.333%': {
+            boxShadow: '9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff, 10014px -10px 0 0 #9880ff;'
+          },
+          '100%': {
+            boxShadow: '9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff, 10014px 0 0 0 #9880ff;'
+          },
+        }
+      },
       height: theme => ({
         'screen-3/4': '75vh',
         'screen/2': '50vh',
